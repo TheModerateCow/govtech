961fcf609a8446a45d5094fb19e4e6c3bb950d65
TheModerateCow <ooibryan988@gmail.com>
2025-02-16 16:50:19 +0800

    Finalised schema

--- schema.sql
+++ schema.sql
@@ -2,11 +2,21 @@ DROP SCHEMA IF EXISTS public CASCADE;
 
 CREATE SCHEMA public;
 
-CREATE TABLE
-    student (student_id SERIAL PRIMARY KEY, name VARCHAR(100));
-
+-- Assumption: 
+-- A student will take multiple course over many semesters but will only take 1 course per semester
+-- A student will only be assigned to one teacher over the full semester period. So student named tom will be assigned to a teacher name John for the whole school duration
+-- It is a good idea to keep track of course names, student names, and teacher names.
+-- The database only tracks the month, regards of the exact date.
 CREATE TABLE
     teacher (teacher_id SERIAL PRIMARY KEY, name VARCHAR(100));
+
+CREATE TABLE
+    student (
+        student_id SERIAL PRIMARY KEY,
+        name VARCHAR(100),
+        teacher_id INT,
+        FOREIGN KEY (teacher_id) REFERENCES teacher (teacher_id)
+    );
 
 -- Changed date to month, focused on what the question is asking for.
 CREATE TABLE
@@ -21,14 +31,12 @@ CREATE TABLE
 CREATE TABLE
     course (
         student_id INT,
-        teacher_id INT,
         sem_no INT,
         year INT,
         course_name VARCHAR(100),
         grade VARCHAR(10),
         PRIMARY KEY (student_id, sem_no, year),
         FOREIGN KEY (student_id) REFERENCES student (student_id),
-        FOREIGN KEY (teacher_id) REFERENCES teacher (teacher_id),
         FOREIGN KEY (sem_no, year) REFERENCES semester (sem_no, year)
     );
 
@@ -66,162 +74,105 @@ VALUES
     (2, 2024, '01', '04');
 
 -- Student id of 1
--- A student can only do one course per sem but its possible for different students to do different courses within each sem
 INSERT INTO
-    COURSE (
-        student_id,
-        teacher_id,
-        sem_no,
-        year,
-        course_name,
-        grade
-    )
+    COURSE (student_id, sem_no, year, course_name, grade)
 VALUES
-    (1, 1, 1, 2021, 'Computer Science 101', 'A+'),
-    (1, 1, 2, 2021, 'Computer Science 102', 'A+'),
-    (1, 1, 1, 2022, 'Math 101', 'A+'),
-    (1, 1, 2, 2022, 'Math 102', 'A+'),
-    (1, 1, 1, 2023, 'Science 101', 'A+'),
-    (1, 1, 2, 2023, 'Science 102', 'A+'),
-    (1, 1, 1, 2024, 'English 101', 'A+'),
-    (1, 1, 2, 2024, 'English 102', 'A+');
+    (1, 1, 2021, 'Computer Science 101', 'A+'),
+    (1, 2, 2021, 'Computer Science 102', 'A+'),
+    (1, 1, 2022, 'Math 101', 'A+'),
+    (1, 2, 2022, 'Math 102', 'A+'),
+    (1, 1, 2023, 'Science 101', 'A+'),
+    (1, 2, 2023, 'Science 102', 'A+'),
+    (1, 1, 2024, 'English 101', 'A+'),
+    (1, 2, 2024, 'English 102', 'A+');
 
--- For student_id = 2 (Teacher 1)
+-- For student_id = 2
 INSERT INTO
-    course (
-        student_id,
-        teacher_id,
-        sem_no,
-        year,
-        course_name,
-        grade
-    )
+    course (student_id, sem_no, year, course_name, grade)
 VALUES
-    (2, 1, 1, 2021, 'Computer Science 101', 'A'),
-    (2, 1, 2, 2021, 'Computer Science 102', 'B+'),
-    (2, 1, 1, 2022, 'Math 101', 'A-'),
-    (2, 1, 2, 2022, 'Math 102', 'B'),
-    (2, 1, 1, 2023, 'Science 101', 'B+'),
-    (2, 1, 2, 2023, 'Science 102', 'A'),
-    (2, 1, 1, 2024, 'English 101', 'A'),
-    (2, 1, 2, 2024, 'English 102', 'B+');
+    (2, 1, 2021, 'Computer Science 101', 'A'),
+    (2, 2, 2021, 'Computer Science 102', 'B+'),
+    (2, 1, 2022, 'Math 101', 'A-'),
+    (2, 2, 2022, 'Math 102', 'B'),
+    (2, 1, 2023, 'Science 101', 'B+'),
+    (2, 2, 2023, 'Science 102', 'A'),
+    (2, 1, 2024, 'English 101', 'A'),
+    (2, 2, 2024, 'English 102', 'B+');
 
--- For student_id = 3 (Teacher 1)
+-- For student_id = 3
 INSERT INTO
-    course (
-        student_id,
-        teacher_id,
-        sem_no,
-        year,
-        course_name,
-        grade
-    )
+    course (student_id, sem_no, year, course_name, grade)
 VALUES
-    (3, 1, 1, 2021, 'History 101', 'B'),
-    (3, 1, 2, 2021, 'History 102', 'B+'),
-    (3, 1, 1, 2022, 'Geography 101', 'A-'),
-    (3, 1, 2, 2022, 'Geography 102', 'B'),
-    (3, 1, 1, 2023, 'Economics 101', 'B+'),
-    (3, 1, 2, 2023, 'Economics 102', 'A'),
-    (3, 1, 1, 2024, 'Philosophy 101', 'A'),
-    (3, 1, 2, 2024, 'Philosophy 102', 'B+');
+    (3, 1, 2021, 'History 101', 'B'),
+    (3, 2, 2021, 'History 102', 'B+'),
+    (3, 1, 2022, 'Geography 101', 'A-'),
+    (3, 2, 2022, 'Geography 102', 'B'),
+    (3, 1, 2023, 'Economics 101', 'B+'),
+    (3, 2, 2023, 'Economics 102', 'A'),
+    (3, 1, 2024, 'Philosophy 101', 'A'),
+    (3, 2, 2024, 'Philosophy 102', 'B+');
 
--- For student_id = 4 (Teacher 1)
+-- For student_id = 4
 INSERT INTO
-    course (
-        student_id,
-        teacher_id,
-        sem_no,
-        year,
-        course_name,
-        grade
-    )
+    course (student_id, sem_no, year, course_name, grade)
 VALUES
-    (4, 1, 1, 2021, 'Biology 101', 'A'),
-    (4, 1, 2, 2021, 'Biology 102', 'A-'),
-    (4, 1, 1, 2022, 'Chemistry 101', 'B+'),
-    (4, 1, 2, 2022, 'Chemistry 102', 'B'),
-    (4, 1, 1, 2023, 'Physics 101', 'A'),
-    (4, 1, 2, 2023, 'Physics 102', 'B+'),
-    (4, 1, 1, 2024, 'English Literature 101', 'A'),
-    (4, 1, 2, 2024, 'English Literature 102', 'A-');
+    (4, 1, 2021, 'Biology 101', 'A'),
+    (4, 2, 2021, 'Biology 102', 'A-'),
+    (4, 1, 2022, 'Chemistry 101', 'B+'),
+    (4, 2, 2022, 'Chemistry 102', 'B'),
+    (4, 1, 2023, 'Physics 101', 'A'),
+    (4, 2, 2023, 'Physics 102', 'B+'),
+    (4, 1, 2024, 'English Literature 101', 'A'),
+    (4, 2, 2024, 'English Literature 102', 'A-');
 
--- For student_id = 5 (Teacher 1)
+-- For student_id = 5
 INSERT INTO
-    course (
-        student_id,
-        teacher_id,
-        sem_no,
-        year,
-        course_name,
-        grade
-    )
+    course (student_id, sem_no, year, course_name, grade)
 VALUES
-    (5, 1, 1, 2021, 'Art 101', 'B+'),
-    (5, 1, 2, 2021, 'Art 102', 'A'),
-    (5, 1, 1, 2022, 'Music 101', 'A-'),
-    (5, 1, 2, 2022, 'Music 102', 'B'),
-    (5, 1, 1, 2023, 'Dance 101', 'B+'),
-    (5, 1, 2, 2023, 'Dance 102', 'A'),
-    (5, 1, 1, 2024, 'Theatre 101', 'A'),
-    (5, 1, 2, 2024, 'Theatre 102', 'B+');
+    (5, 1, 2021, 'Art 101', 'B+'),
+    (5, 2, 2021, 'Art 102', 'A'),
+    (5, 1, 2022, 'Music 101', 'A-'),
+    (5, 2, 2022, 'Music 102', 'B'),
+    (5, 1, 2023, 'Dance 101', 'B+'),
+    (5, 2, 2023, 'Dance 102', 'A'),
+    (5, 1, 2024, 'Theatre 101', 'A'),
+    (5, 2, 2024, 'Theatre 102', 'B+');
 
--- For student_id = 6 (Teacher 2)
+-- For student_id = 6
 INSERT INTO
-    course (
-        student_id,
-        teacher_id,
-        sem_no,
-        year,
-        course_name,
-        grade
-    )
+    course (student_id, sem_no, year, course_name, grade)
 VALUES
-    (6, 2, 1, 2021, 'Economics 101', 'A'),
-    (6, 2, 2, 2021, 'Economics 102', 'A-'),
-    (6, 2, 1, 2022, 'Statistics 101', 'B'),
-    (6, 2, 2, 2022, 'Statistics 102', 'B+'),
-    (6, 2, 1, 2023, 'Business 101', 'A'),
-    (6, 2, 2, 2023, 'Business 102', 'A'),
-    (6, 2, 1, 2024, 'Marketing 101', 'A'),
-    (6, 2, 2, 2024, 'Marketing 102', 'A-');
+    (6, 1, 2021, 'Economics 101', 'A'),
+    (6, 2, 2021, 'Economics 102', 'A-'),
+    (6, 1, 2022, 'Statistics 101', 'B'),
+    (6, 2, 2022, 'Statistics 102', 'B+'),
+    (6, 1, 2023, 'Business 101', 'A'),
+    (6, 2, 2023, 'Business 102', 'A'),
+    (6, 1, 2024, 'Marketing 101', 'A'),
+    (6, 2, 2024, 'Marketing 102', 'A-');
 
--- For student_id = 7 (Teacher 2)
+-- For student_id = 7
 INSERT INTO
-    course (
-        student_id,
-        teacher_id,
-        sem_no,
-        year,
-        course_name,
-        grade
-    )
+    course (student_id, sem_no, year, course_name, grade)
 VALUES
-    (7, 2, 1, 2021, 'Computer Science 101', 'B'),
-    (7, 2, 2, 2021, 'Computer Science 102', 'B+'),
-    (7, 2, 1, 2022, 'Math 101', 'B-'),
-    (7, 2, 2, 2022, 'Math 102', 'B'),
-    (7, 2, 1, 2023, 'Science 101', 'B+'),
-    (7, 2, 2, 2023, 'Science 102', 'B'),
-    (7, 2, 1, 2024, 'English 101', 'B+'),
-    (7, 2, 2, 2024, 'English 102', 'B');
+    (7, 1, 2021, 'Computer Science 101', 'B'),
+    (7, 2, 2021, 'Computer Science 102', 'B+'),
+    (7, 1, 2022, 'Math 101', 'B-'),
+    (7, 2, 2022, 'Math 102', 'B'),
+    (7, 1, 2023, 'Science 101', 'B+'),
+    (7, 2, 2023, 'Science 102', 'B'),
+    (7, 1, 2024, 'English 101', 'B+'),
+    (7, 2, 2024, 'English 102', 'B');
 
--- For student_id = 8 (Teacher 2)
+-- For student_id = 8
 INSERT INTO
-    course (
-        student_id,
-        teacher_id,
-        sem_no,
-        year,
-        course_name,
-        grade
-    )
+    course (student_id, sem_no, year, course_name, grade)
 VALUES
-    (8, 2, 1, 2021, 'History 101', 'A'),
-    (8, 2, 2, 2021, 'History 102', 'A-'),
-    (8, 2, 1, 2022, 'Geography 101', 'B'),
-    (8, 2, 2, 2022, 'Geography 102', 'B+'),
-    (8, 2, 1, 2023, 'Economics 101', 'A'),
-    (8, 2, 2, 2023, 'Economics 102', 'A-'),
-    (8, 2, 1, 2024, 'Philosophy 101', 'B'),
-    (8, 2, 2, 2024, 'Philosophy 102', 'B+');
+    (8, 1, 2021, 'History 101', 'A'),
+    (8, 2, 2021, 'History 102', 'A-'),
+    (8, 1, 2022, 'Geography 101', 'B'),
+    (8, 2, 2022, 'Geography 102', 'B+'),
+    (8, 1, 2023, 'Economics 101', 'A'),
+    (8, 2, 2023, 'Economics 102', 'A-'),
+    (8, 1, 2024, 'Philosophy 101', 'B'),
+    (8, 2, 2024, 'Philosophy 102', 'B+');
